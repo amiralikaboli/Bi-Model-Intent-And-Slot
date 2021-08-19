2780decdc1a329ffad7259b672e8dd37b5cccce3
AmirAli Kaboli <41191410+amiralikaboli@users.noreply.github.com>
2021-08-19 21:45:20 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,15 @@
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-total_epoch = 500
-max_len = 50
-batch = 16
+total_epoch = 10
+max_len = 32
+batch = 64
 learning_rate = 0.001
 DROPOUT = 0.2 # 0.2, 0.3, 0.4
 
 
-embedding_size = 300
+embedding_size = 100
 lstm_hidden_size = 200
 
 
-train_file = 'data/train_dev'
-test_file = 'data/test'
-
-vocab_intent_file = 'data/vocab.intent'
-vocab_slot_file = 'data/vocab.slot'
+data_path = "../../../Datasets/MultiWOZ_2.2"
